a4d72957181219b147bc217eb4cb02fcda33bb81
Camden <c@camdenmecklem.com>
2025-01-08 14:33:38 -0500

    Migrate to Astro and include `RecordList` example (#6)
    * migrate to Astro
    
    * get RecordList working
    
    * include build in start script

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import EditionCrafter from "@cu-mkp/editioncrafter";
-import config from "../data/config.json";
-
-const App = () => {
-  return <EditionCrafter {...config} />;
-};
-
-export default App;
